225f3bf7e74aa9fffa4e5f2be50ceeef8a936820
srishti-3 <srishti.8055@iitg.ac.in>
2025-06-16 12:03:08 +0530

    Create ApplicationDetails.jsx

---
+++ frontend/src/pages/ApplicationDetails.jsx
@@ -0,0 +1,14 @@
+// src/pages/ApplicationDetails.jsx
+import React from 'react';
+
+const ApplicationDetails = () => {
+  return (
+    <div>
+      <h1>Application Details Page</h1>
+    </div>
+  );
+};
+
+export default ApplicationDetails;
+
+//this needs to be updated
